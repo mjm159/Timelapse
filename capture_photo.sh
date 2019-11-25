f11239386f1f2d8a37953ed46d84e8166f713db4
Michael Malocha <mmalocha13@gmail.com>
2019-11-24 18:43:48 -0800

    add basic bash script

---
+++ capture_photo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Capture photo
+RESOLUTION="1920x1080"
+HOSTNAME=$(uname -n)
+DATE=$(date +"%Y-%m-%d_%H-%M-%S_%Z")
+IMAGE_PATH=$(cd ~;pwd)/images
+IMG_FILENAME=$HOSTNAME\_$DATE.jpg
+IMG_FILEPATH=$IMAGE_PATH/$IMG_FILENAME
+fswebcam -r $RESOLUTION --no-banner $IMG_FILEPATH
+
+# Upload
+source $(cd ~;pwd)/Timelapse/env/bin/activate
+python3 upload_to_bucket.py $IMG_FILEPATH $IMG_FILENAME
